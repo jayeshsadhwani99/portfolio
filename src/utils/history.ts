770988a411cae6c17779543a9954bfefd0877a55
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2023-08-06 01:28:14 +0530

    add more experience

--- src/utils/history.ts
+++ src/utils/history.ts
@@ -5,9 +5,19 @@ const history = [
     company: "Multiple Roles",
   },
   {
+    year: 2022,
+    role: "Lemon8",
+    company: "Back-End Developer",
+  },
+  {
+    year: 2022,
+    role: "TheFluencer",
+    company: "Full Stack Developer",
+  },
+  {
     year: 2021,
     role: "Respct",
-    company: "Web3 Develoepr",
+    company: "Web3 Developer",
   },
   {
     year: 2021,
